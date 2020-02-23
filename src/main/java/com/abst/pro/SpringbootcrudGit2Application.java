f55a21806f34ef4d0b6e053466f4afa1a7146a06
SnehaGomase <snehagomasee@gmail.com>
2020-02-23 20:17:01 +0530

    New Spring Boot Project Added

---
+++ src/main/java/com/abst/pro/SpringbootcrudGit2Application.java
@@ -0,0 +1,13 @@
+package com.abst.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcrudGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcrudGit2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/abst/pro/SpringbootcrudGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abst.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrudGit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
